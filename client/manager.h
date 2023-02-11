aa349b144d8c89604a5a0449c39380d9d997a222
Nicolau Alff <nicolau.alff@gmail.com>
2023-02-11 03:24:08 -0300

    firsts functions

--- client/manager.h
+++ client/manager.h
@@ -6,6 +6,9 @@
 #define CMD 2
 #define BUFFER_SIZE 256
 
+#ifndef COMMUNICATION_MANAGEMENT
+#define COMMUNICATION_MANAGEMENT
+
 typedef struct __packet {
     uint16_t type;          //Tipo do pacote (p.ex. DATA | CMD)
     uint16_t seqn;          //Número de sequência
@@ -17,3 +20,13 @@ typedef struct __packet {
 typedef struct __client_thread_args {
     int sockfd;                 // Socket de comunicação
 } client_thread_args;
+
+// metodos comuns a servidor e cliente
+int read_packet(int socketID, packet* pack, char *buffer); // 
+int send_packet(int socketID, packet* pack);
+
+
+// metodos do cliente
+int connect_to_server (int socketID, char* port_number)
+
+#endif
