d131b9c41e18fcf365203ee5643bbc0d8ec53e0e
RichardLeal <you@example.com>
2023-02-11 23:35:55 -0300

    Update sync_client_directory function

--- Dropbox/client.c
+++ Dropbox/client.c
@@ -1,25 +1,41 @@
+void *sync_client_directory() {
 
-void *monitoringClientDirectory() {
+    char file_path[200]; // path
 
-    char file_path[200];
-
-    char buffer[BUFFER_LENGTH];
-    int notify_read_length = 0;
-    int reading_notify = 0;
+    char buffer[BUFFER_LENGTH]; //buffer
+    int notify_read_length = 0; //length
+    int notify_reading = 0; // i
 
     //create_sync_sock();
     //get_all_files();
 
     while(TRUE) {
+        // Reading of directory events
         notify_read_length = read(notifier, buffer, BUFFER_LENGTH);
         if(notify_read_length < 0)
             perror("ERROR: Monitoring client directory");
 
-        while(reading_notify < notify_read_length) {
-            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+        while(notify_reading < notify_read_length) {
+            struct inotify_event *event = (struct inotify_event*) &buffer[notify_reading];
             if(event->len) {
+                // Concatenation file path
+                strcpy(path, directory); strcat(path, "/"); strcat(path, event->name);
+                if(event->mask & IN_CLOSE_WRITE || event->mask & IN_CREATE || event->mask & IN_MOVED_TO) {
+					if(exists(path) && (event->name[0] != '.'))
+						//upload_file(path, sync_socket);
+                    else
+                        perror("ERROR: Sync client directory - IN_CLOSE_WRITE | IN_CREATE | IN_MOVED_TO");
+                }
+                if (event->mask & IN_DELETE || event->mask & IN_MOVED_FROM){
+					if(event->name[0] != '.')
+					    //delete_file_request(path, sync_socket);
+                    else
+                        perror("ERROR: Sync client directory - IN_DELETE | IN_MOVED_FROM");
+				}
             }
+            notify_reading += EVENT_SIZE + event->len;
         }
-
+        notify_reading = 0;
+        sleep(10);
     }
 }
--- Dropbox/client.h
+++ Dropbox/client.h
@@ -1,11 +1,34 @@
+// Libraries
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <pwd.h>
+#include <sys/inotify.h>
 
+// Contants
+#define EVENT_SIZE ( sizeof (struct inotify_event) )
+#define BUFFER_LENGTH ( 1024 * ( EVENT_SIZE + 16 ) )
 
+// Headers
 #ifndef CLIENTE
 #define CLIENTE
 
 #define TRUE 1
 #define FALSE 0
 
-void *monitoringClientDirectory();
+
+void *sync_client_directory(); // void *sync_thread();
 
 #endif
