2a376cec8e6887f3153590051850834e38bb4033
Ramos, Richard <Richard_Ramos1@dell.com>
2023-02-13 16:03:18 -0300

    Add communication.c

---
+++ client/comunication.c
@@ -0,0 +1,26 @@
+#include "communication.h"
+
+int open_connection_with_server(char *host, int port) {
+
+} //int connect_server (char *host, int port)
+
+void close_connection_with_server(){
+// void close_connection();
+}
+
+void download_files() {
+} // void get_all_files();
+
+void download_file(char *file){
+
+} // void get_file(char *file)
+
+void upload_file(char *client_file_path, char *file_name, int socket) {
+} // void upload_file(char *file, int socket);
+
+void delete_file(char *file_name, int socket) {
+} // void delete_file_request(char* file, int socket);
+
+void list_files() {
+
+} //void show_files()
